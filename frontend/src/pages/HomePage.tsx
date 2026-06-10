1d1512c6820babb57611f9697d9710b7482f94ab
andreibukov <andreibukov15@gmail.com>
2026-06-10 21:44:01 +0300

    Clean up unused project files

--- frontend/src/pages/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-    return (
-        <div>
-            <h1>Football Transfer Advisor</h1>
-            <p>Intelligent Multi-Agent Football Transfer Recommendation System</p>
-        </div>
-    );
-}
